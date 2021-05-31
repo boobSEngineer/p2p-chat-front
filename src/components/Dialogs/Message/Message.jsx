507acacb5747e9cd63ea4c4dcfaf75ff0d115b2f
Helen <helenz.eltech@gmail.com>
2021-06-01 00:18:05 +0300

    time

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,17 +3,21 @@ import s from './Message.module.css';
 
 
 const Message = (props) => {
-    debugger
+    let messageDate = new Date(props.message.timestamp);
     return (
         <div className={[
             s.message,
             s.text,
             `${props.message.senderUid === props.me ? s.mine : ''}`,
         ].join(" ")}>
-
             <div className={s.bubble_container}>
                 <div className={[s.bubble].join(" ")} title={Message}>
                     {props.message.text}
+                    {
+                        <div className={s.timestamp}>
+                            { messageDate.getHours() + ":" + messageDate.getMinutes() }
+                        </div>
+                    }
                 </div>
             </div>
         </div>
